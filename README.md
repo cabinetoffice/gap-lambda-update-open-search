43d12922ca0b4500a4bd0526e2b4ff1fee1f1581
Dylan Wright <dylan.wright@and.digital>
2024-04-22 21:39:34 +0100

    Initial commit, adds functionality to update elastic search

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # gap-lambda-update-open-search
+
+This lambda is invoked by gap-find-admin-api when publishing or un-publishing adverts on contentful.
+
+The purpose of this lambda is to:
+
+- fetch publish grants via contentful-management by contentful entry id given in the request
+- update elastic search via http using the contentful entry result
+
+## Invocation
+
+The request requires the follwoing message payload:
+
+{
+type: 'ADD' || 'REMOVE'
+contentfulEntryId: string
+}
---
+++ index.mjs
@@ -0,0 +1,94 @@
+import fetch from 'node-fetch';
+import contentful from 'contentful-management';
+
+const openSearchConfig = {
+  username: process.env.OPEN_SEARCH_USERNAME,
+  password: process.env.OPEN_SEARCH_PASSWORD,
+  url: process.env.OPEN_SEARCH_URL,
+  indexUri: process.env.OPEN_SEARCH_INDEX_URI,
+};
+
+const contentfulConfig = {
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  environmentId: process.env.CONTENTFUL_ENVIRONMENT_ID,
+};
+
+const client = contentful.createClient(
+  {
+    accessToken: contentfulConfig.accessToken,
+  },
+  {
+    type: 'plain',
+    defaults: {
+      spaceId: contentfulConfig.spaceId,
+      environmentId: contentfulConfig.environmentId,
+    },
+  }
+);
+
+const getGrantById = async (contentfulEntryId) => {
+  const { items } = await client.entry.getPublished({
+    query: {
+      'sys.id': contentfulEntryId,
+      content_type: 'grantDetails',
+    },
+  });
+
+  if (!items || !items.length) {
+    throw new Error(`No published grant found in contentful with id ${id}`);
+  }
+
+  // Should always return one result as the id is unique
+  console.log(`Found ${items.length} published grant(s) in contentful`);
+  return items[0];
+};
+
+const updateElasticIndex = async (contentfulEntry, action) => {
+  const auth = openSearchConfig.username + ':' + openSearchConfig.password;
+  const authHeader = 'Basic ' + btoa(auth);
+  const url =
+    openSearchConfig.url + openSearchConfig.indexUri + contentfulEntry.sys.id;
+
+  const ACTIONS = {
+    ADD: 'PUT',
+    REMOVE: 'DELETE',
+  };
+
+  const method = ACTIONS[action];
+  if (!method)
+    throw new Error(`Action not recognised for action type ${action}`);
+
+  const body = JSON.stringify(contentfulEntry);
+
+  console.log(
+    `Updating elastic index for grant ${contentfulEntry.fields.grantName['en-US']}, with contentful entry: \n ${body}`
+  );
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      Authorization: authHeader,
+    },
+    body: body,
+  });
+
+  if (!response || !response.ok) {
+    const errorMessage = await response.text();
+    throw new Error(
+      `Failed to create an elastic index entry for ad ${contentfulEntry.sys.id} in open search: ${errorMessage}`
+    );
+  } else {
+    console.log(
+      `Successfully updated elastic index for grant ${contentfulEntry.fields.grantName['en-US']}`
+    );
+  }
+};
+
+export const handler = async (message) => {
+  const contentfulEntryId = message.contentfulEntryId;
+  const grant = await getGrantById(contentfulEntryId);
+  await updateElasticIndex(grant, message.type);
+
+  return { statusCode: 200 };
+};
